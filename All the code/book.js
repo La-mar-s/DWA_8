71829785b2ac0c612bf4991fb8a36686fb41f689
La-mar-s <sony.lamar.1995@gmail.com>
2023-09-05 22:34:13 +0200

    finished DWA8

---
+++ All the code/book.js
@@ -0,0 +1,22 @@
+import {books, authors, genres, BOOKS_PER_PAGE} from './data.js';
+
+function createPreviewElement({ author, id, image, title }) {
+  const element = document.createElement("button");
+  element.classList = "preview";
+  element.setAttribute("data-preview", id);
+
+  element.innerHTML = `
+        <img
+            class="preview__image"
+            src="${image}"
+        />
+        
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${authors[author]}</div>
+        </div>
+    `;
+
+  return element;
+}
+export {createPreviewElement}
